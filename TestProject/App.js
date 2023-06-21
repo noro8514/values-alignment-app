62782dd846efaa071db2f2fc825064ca0b8d8872
Kai Franks <123043443+TinyStranger21@users.noreply.github.com>
2023-06-20 18:56:15 -0600

    Update App.js

--- TestProject/App.js
+++ TestProject/App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(255,255,255)',
     alignItems: 'center',
     justifyContent: 'center',
   },
