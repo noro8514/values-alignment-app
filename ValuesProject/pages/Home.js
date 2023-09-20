de5aceffceaf86e02a67514cf645cb377d83280c
shwi6838 <shwi6838@colorado.edu>
2023-09-20 15:34:15 -0600

    Home Screen
    Added basic achievements

--- ValuesProject/pages/Home.js
+++ ValuesProject/pages/Home.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CalendarPicker from 'react-native-calendar-picker';
+import CircularProgress from 'react-native-circular-progress-indicator';
 
 var moment = require('moment');
 
@@ -28,13 +29,74 @@ const HomeScreen = ({navigation, route}) => {
     return (
         <View style={styles.container}>
             <Text>Home Screen</Text>
+            <Text> Good Morning, (Name)</Text>
+            {/* Calendar */}
+            <View style={styles.calendarcontainer}>
+            <Text>Daily Check In</Text>
             <CalendarPicker 
                 previousTitle="<"
                 nextTitle=">"
                 enableDateChange={false}
                 dayShape={'square'}
                 customDatesStyles={customDatesStyles}
+                width={300}
             />
+            </View>
+            {/* Achievements */}
+            <View style={styles.overallachievementscontainer}>
+            <Text> Upcoming Achievements</Text>
+            <View style={styles.achievementscontainer}>
+                {/* Need to use a circular status bar for the achievements*/}
+                <View style={styles.achievement}>
+                    <CircularProgress
+                        value={50}
+                        radius={40}
+                        activeStrokeColor={'#00ff00'}
+                        inActiveStrokeColor={'#ff0000'}
+                        inActiveStrokeOpacity={0.5}
+                        inActiveStrokeWidth={5}
+                        activeStrokeWidth={10}
+                        duration={1000}
+                        textStyle={{fontSize: 20}}
+                        onAnimationComplete={() => console.log('onAnimationComplete')}
+                    />
+                    <Text> Achievement 1</Text>
+                    <Text>Description</Text>
+                </View>
+                <View style={styles.achievement}>
+                    <CircularProgress
+                        value={80}
+                        radius={40}
+                        activeStrokeColor={'#00ff00'}
+                        inActiveStrokeColor={'#ff0000'}
+                        inActiveStrokeOpacity={0.5}
+                        inActiveStrokeWidth={5}
+                        activeStrokeWidth={10}
+                        duration={1000}
+                        textStyle={{fontSize: 20}}
+                        onAnimationComplete={() => console.log('onAnimationComplete')}
+                    />
+                    <Text> Achievement 2</Text>
+                    <Text>Description</Text>
+                </View>
+                <View style={styles.achievement}>
+                    <CircularProgress
+                        value={10}
+                        radius={40}
+                        activeStrokeColor={'#00ff00'}
+                        inActiveStrokeColor={'#ff0000'}
+                        inActiveStrokeOpacity={0.5}
+                        inActiveStrokeWidth={5}
+                        activeStrokeWidth={10}
+                        duration={1000}
+                        textStyle={{fontSize: 20}}
+                        onAnimationComplete={() => console.log('onAnimationComplete')}
+                    />
+                    <Text> Achievement 3</Text>
+                    <Text>Description</Text>
+                </View>
+            </View>
+            </View>
         </View>
     );
 }
@@ -46,6 +108,35 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    calendarcontainer: {
+        width: '80%',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10,
+        marginTop: 10,
+    },
+    overallachievementscontainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+    },
+    achievementscontainer: {
+        flex: 2/3,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+    },
+    achievement: {
+        marginLeft: 10,
+        marginRight: 10,
     },
 });
 
