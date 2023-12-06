71c5471255b922b4c2af3e9f3cf07886db39a5ea
nazhonem <namo2624@colorado.edu>
2023-12-06 11:44:00 -0700

    story container and navigation

--- ValuesProject/pages/Story.js
+++ ValuesProject/pages/Story.js
@@ -1,37 +1,84 @@
-import React from 'react';
-import { View, FlatList, Image, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, View, FlatList, Image, Text, StyleSheet, Dimensions} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const storiesData = [
-  // Data for stories: You can fetch this from an API or define locally
-  { id: 1, username: 'user1', imageUrl: 'https://example.com/image1.jpg' },
-  { id: 2, username: 'user2', imageUrl: 'https://example.com/image2.jpg' },
-  // Add more story objects as needed
-];
+const { height } = Dimensions.get('window');
+export function useHideBottomBar() {
+    const navigation = useNavigation();
 
-const Story = ({ username, imageUrl }) => (
-  <View style={styles.storyContainer}>
-    <Image source={{ uri: imageUrl }} style={styles.storyImage} />
-    <Text style={styles.username}>{username}</Text>
-  </View>
-);
+    useEffect(() => {
+        navigation.getParent()?.setOptions({ tabBarStyle: { display: 'none' }, tabBarVisible: false });
+        return () =>
+            navigation.getParent()?.setOptions({ tabBarStyle: undefined, tabBarVisible: undefined });
+    }, [navigation]);
+}
+
+// const storiesData = [
+//   // Data for stories: You can fetch this from an API or define locally
+//   { id: 1, username: 'user1', imageUrl: 'https://example.com/image1.jpg' },
+//   { id: 2, username: 'user2', imageUrl: 'https://example.com/image2.jpg' },
+//   // Add more story objects as needed
+// ];
+
+// const Story = ({ username, imageUrl }) => (
+//   <View style={styles.storyContainer}>
+//     <Image source={{ uri: imageUrl }} style={styles.storyImage} />
+//     <Text style={styles.username}>{username}</Text>
+//   </View>
+// );
 
 const StoryScreen = () => {
-  return (
-    <FlatList
-      data={storiesData}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <Story username={item.username} imageUrl={item.imageUrl} />
-      )}
-    />
-  );
+    useHideBottomBar();
+    const navigation = useNavigation();
+
+    const colorsData = ['black', 'blue', 'green']; // Define the colors to switch between
+    const [currentColorIndex, setCurrentColorIndex] = useState(0);
+
+    useEffect(() => {
+        // Function to switch to the next color after a set duration (e.g., 5 seconds)
+        var nextIndex = currentColorIndex;
+        const timer = setTimeout(() => {
+            nextIndex += 1;
+            if(nextIndex < colorsData.length){
+                setCurrentColorIndex(nextIndex);
+            }
+            else {
+                navigation.navigate('Home1')
+            }
+        }, 5000); // Change the delay (in milliseconds) to switch to the next color
+    
+        // Clear the timer when the component unmounts or when the current color index changes
+        return () => clearTimeout(timer);
+    }, [currentColorIndex]);
+    
+    const currentColor = colorsData[currentColorIndex];
+    
+    return (
+        // <FlatList
+        // data={storiesData}
+        // horizontal
+        // showsHorizontalScrollIndicator={false}
+        // keyExtractor={(item) => item.id.toString()}
+        // renderItem={({ item }) => (
+        //     <Story username={item.username} imageUrl={item.imageUrl} />
+        // )}
+        // />
+        
+        <View style={[styles.storyContainer, { backgroundColor: currentColor }]}>
+            <Button
+                title="X"
+                onPress={() => navigation.navigate('Home1')}
+            />
+            <Text style={styles.text}>Current Color: {currentColor}</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
   storyContainer: {
-    margin: 10,
+    flex: 1,
+    height: height,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   storyImage: {
